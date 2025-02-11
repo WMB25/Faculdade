f88419568801f059f3510db2a7c55d729fe1089f
WalisonM.B <walisonmourabraz@gmail.com>
2025-02-11 16:00:55 -0300

    Atualização

--- CalendarioDaComputação/CalendarioDaComputação/Program.cs
+++ CalendarioDaComputação/CalendarioDaComputação/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography;
 
 public class Calendario
 {
@@ -7,31 +9,53 @@ public class Calendario
     {
        /* Console.WriteLine(DateTime.UtcNow);*/
         var data = new DateTime();
+        var formatoDeEscolha = "";
         switch (formato)
         {
             case "DMY":
-                Console.WriteLine("");
-                var formatoDeEscolha = String.Format("{0: dd, MM, yyyy HH:mm:aa ff z}", data);
-                Console.WriteLine(formatoDeEscolha);
-
+                formatoDeEscolha = data.ToString("{0: dd / MM / yyyy HH:mm:aa ff z}");
+            break;
+            case "MDY":
+                formatoDeEscolha = data.ToString("{0: MM / dd / yyyy HH:mm:aa}");                
+            break;
+            case "YMD":
+                formatoDeEscolha = data.ToString("{0: yyyy/ MM / dd HH:mm:aa}");               
+            break;
+            case "ISO":
+                formatoDeEscolha = data.ToString("{0: dd, MM, yyyy HH:mm:aa}");
+            break;
+            case "RRR":
+                formatoDeEscolha = data.ToString("{0:r}");
             break;
         }
-
+        Console.WriteLine(formatoDeEscolha);
     }
-
-
-
     static void Main(string[] args)
     {
         Console.WriteLine("Calendario Da Computaria\n");
-        Console.WriteLine("Digite o formato que desenja visualizar:\nMDY: Mês, Dia, Ano\nDMY: Dia, Mês, Ano\nYMD: Ano, Mês, Dia\nISO: Ano - Mês - Dia\nDigite o formato: (EX: DMY)");
-        string d = Console.ReadLine();
-        Calendario test = new Calendario();
-        test.ConfiguracaoCalendario(d);
+        string[] formatoDisponivel = new string[] { "RRR", "MDY", "DMY", "YMD", "ISO" };  
+        //transformar em Funçao!
+        Console.WriteLine("Digite o formato que desenja visualizar:\nMDY: Mês, Dia, Ano\nDMY: Dia, Mês, Ano\nYMD: Ano, Mês, Dia\nISO: Ano - Mês - Dia\nRRR: S, Dia, Mês, Ano\nDigite o formato: (EX: DMY)");
+        string formato = Console.ReadLine().ToUpper();
 
-
-
-
-
+        bool FormatoVaido = false;
+        foreach(string forma in formatoDisponivel)
+        {
+            if(formato == forma)
+            {
+                FormatoVaido = true;
+                break;
+            }
+        }
+        if (FormatoVaido)
+        {
+            Calendario calendario = new Calendario();
+            calendario.ConfiguracaoCalendario(formato);
+        }
+        else
+        {
+            Console.WriteLine("Digite um Formato Válido");
+            return;
+        }
     }
 }
